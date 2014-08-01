02a7a764422947790983c4ba7e1ee479839b65bb
Sormano <jeroen.sormani@gmail.com>
2014-08-01 18:44:40 +0200

    Spelling fixes

--- woocommerce-availability-chart.php
+++ woocommerce-availability-chart.php
@@ -10,16 +10,16 @@ Text Domain: woocommerce-availability-chart
 
  * Copyright Grow Development
  *
- *     This file is part of Woocommerce Availability Chart,
+ *     This file is part of WooCommerce Availability Chart,
  *     a plugin for WordPress.
  *
- *     Woocommerce Availability Chart is free software:
+ *     WooCommerce Availability Chart is free software:
  *     You can redistribute it and/or modify it under the terms of the
  *     GNU General Public License as published by the Free Software
  *     Foundation, either version 3 of the License, or (at your option)
  *     any later version.
  *
- *     Woocommerce Availability Chart is distributed in the hope that
+ *     WooCommerce Availability Chart is distributed in the hope that
  *     it will be useful, but WITHOUT ANY WARRANTY; without even the
  *     implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
  *     PURPOSE. See the GNU General Public License for more details.
